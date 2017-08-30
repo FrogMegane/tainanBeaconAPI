3c1de1c9700fcb473c52f6a8b1e5d17f1c558099
東成 釋 <qwer124941@gmail.com>
2017-08-31 04:53:28 +0800

    add View

---
+++ tainanBeaconAPI/tainanBeaconAPI/Controllers/ExhibitionsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using tainanBeaconAPI.Models;
+
+namespace tainanBeaconAPI.Controllers
+{
+    public class ExhibitionsController : Controller
+    {
+        private TainanBeaconApp db = new TainanBeaconApp();
+
+        // GET: Exhibitions
+        public ActionResult Index()
+        {
+            var exhibitions = db.Exhibitions.Include(e => e.Site);
+            return View(exhibitions.ToList());
+        }
+
+        // GET: Exhibitions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            if (exhibition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exhibition);
+        }
+
+        // GET: Exhibitions/Create
+        public ActionResult Create()
+        {
+            ViewBag.SiteId = new SelectList(db.Sites, "Id", "Name");
+            return View();
+        }
+
+        // POST: Exhibitions/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SiteId,Name,Introduction,StartDate,EndDate,OpenTime,CloseTime,TicketInfo,Note,Icon,Photo")] Exhibition exhibition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Exhibitions.Add(exhibition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SiteId = new SelectList(db.Sites, "Id", "Name", exhibition.SiteId);
+            return View(exhibition);
+        }
+
+        // GET: Exhibitions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            if (exhibition == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SiteId = new SelectList(db.Sites, "Id", "Name", exhibition.SiteId);
+            return View(exhibition);
+        }
+
+        // POST: Exhibitions/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SiteId,Name,Introduction,StartDate,EndDate,OpenTime,CloseTime,TicketInfo,Note,Icon,Photo")] Exhibition exhibition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(exhibition).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SiteId = new SelectList(db.Sites, "Id", "Name", exhibition.SiteId);
+            return View(exhibition);
+        }
+
+        // GET: Exhibitions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            if (exhibition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exhibition);
+        }
+
+        // POST: Exhibitions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            db.Exhibitions.Remove(exhibition);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
